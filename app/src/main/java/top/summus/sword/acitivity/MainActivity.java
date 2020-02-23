a7bd13a5de17a6d2dbe64dbcb036f95639f48e54
Summuss <45835959+Summuss@users.noreply.github.com>
2020-02-23 15:54:39 +0800

    init package

--- app/src/main/java/top/summus/sword/acitivity/MainActivity.java
+++ app/src/main/java/top/summus/sword/acitivity/MainActivity.java
@@ -1,8 +1,10 @@
-package top.summus.sword;
+package top.summus.sword.acitivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import top.summus.sword.R;
 
 public class MainActivity extends AppCompatActivity {
 
