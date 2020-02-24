dc672068b5b6216bdb7d4a37a7556a896d8b736a
Summuss <45835959+Summuss@users.noreply.github.com>
2020-02-24 18:54:44 +0800

    add a recyclerView for TestFragment

--- app/src/main/java/top/summus/sword/fragment/TestFragment.java
+++ app/src/main/java/top/summus/sword/fragment/TestFragment.java
@@ -4,7 +4,9 @@ package top.summus.sword.fragment;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +14,14 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import top.summus.sword.R;
+import top.summus.sword.databinding.FragmentTestBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TestFragment extends Fragment {
 
+    private FragmentTestBinding binding;
 
     public TestFragment() {
         // Required empty public constructor
@@ -27,17 +31,18 @@ public class TestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_test, container, false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        binding.testRecycler.setLayoutManager(layoutManager);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        String s = getArguments().getString("name");
-        TextView textView = getView().findViewById(R.id.textView2);
-        textView.setText(s);
 
     }
 }
